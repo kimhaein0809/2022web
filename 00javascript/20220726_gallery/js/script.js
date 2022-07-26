3ced7d9ce312c0d9b08228e78587148cc18d2339
kimhaein0809 <laura0809@naver.com>
2022-07-26 15:11:49 +0900

    fd

---
+++ 00javascript/20220726_gallery/js/script.js
@@ -0,0 +1,26 @@
+$("itemWrap>li").click(function(){
+    console.log($(this).index());
+    let i = $(this).index();
+    $("#image>p").hide();
+    $("#image>p").eq(i).show();
+})
+
+let current = 0;
+$(".right").click(function(){
+    current++;
+    if(current == 3){current = 2}
+    console.log(current);
+    view(current)
+})
+
+$(".left").click(function(){
+    current--;
+    if(current < 0){current = 0}
+    view(current)
+})
+
+function view(current){
+    let value = current * -760;
+    $(".itemWrap").stop().animate({"left":value},300)
+}
+
