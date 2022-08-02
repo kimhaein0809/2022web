d845a2e1fa49b510c80f05a324a704b99ead15af
kimhaein0809 <laura0809@naver.com>
2022-08-02 12:18:04 +0900

    fgdg

--- 00javascript/20220729_barmenu/js/script.js
+++ 00javascript/20220729_barmenu/js/script.js
@@ -1,1 +1,36 @@
-console.log("test")
+// $(".navbar ul li").mouseover(function(){
+//     let i = $(this).index();
+//     console.log(i);
+
+//     let value = i * 240;
+//     $(".bar").css("left",value)
+// });
+
+
+// menu 0 1 2 3 4
+function nowMenu(menu){
+    console.log(menu)
+
+    if(menu == "main"){
+        $(".navbar ul").find("li").mouseover(over);
+        $(".navbar ul").mouseout(out);
+    }else{
+        $(".navbar li").eq(menu).addClass("active");
+        $(".bar").css({left:menu*240,opacity:1});
+
+        $(".navbar ul").find("li").mouseover(over);
+
+        $(".navbar ul").mouseout(function(){
+            $(".bar").css({left:menu*240,opacity:1});
+        });
+    }
+}
+
+function over(){
+    let i = $(this).index();
+    let value = i * 240;
+    $(".bar").css({left:value,opacity:1})
+}
+function out(){
+    $(".bar").css({opacity:0})
+}
