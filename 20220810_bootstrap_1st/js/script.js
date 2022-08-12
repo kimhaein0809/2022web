e451409a83f6334fe16d5a3d0247d115b9a00689
kimhaein0809 <laura0809@naver.com>
2022-08-12 16:40:53 +0900

    ads

--- 20220810_bootstrap_1st/js/script.js
+++ 20220810_bootstrap_1st/js/script.js
@@ -36,14 +36,31 @@ $("#oslide2").owlCarousel({
     autoplay:true,
     margin:30,
     responsive:{
-        0:{
-            items:1
-        },
-        760:{
-            items:2
-        },
-        990:{
-            items:4
-        }
+        0:{items:1},
+        760:{items:2},
+        990:{items:4}
     }
+})
+
+$(".view .vPopup").magnificPopup({
+    type:"image",
+    gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    }
+})
+
+console.log(window.innerHeight);
+const lazyLoad = document.querySelectorAll(".lazy-load");
+window.addEventListener("scroll",function(){
+    lazyLoad.forEach(function(item){
+        // console.log(item);
+        const rect = item.getBoundingClientRect().top
+        console.log(rect);
+        if(rect <= window.innerHeight){
+            item.classList.add("fade")
+        }
+    })
+
 })
