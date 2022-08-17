df8efcabdd760d6e4b7c0d71768b2161579bf511
kimhaein0809 <laura0809@naver.com>
2022-08-17 18:19:35 +0900

    ds

--- 20220812_bootstrap_2st/js/script.js
+++ 20220812_bootstrap_2st/js/script.js
@@ -1,1 +1,14 @@
-console.log(test)
+new Swiper('.swiper',{
+    loop:true,
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    }
+})
+
+$(".banner1 > .row > div").hover(function(){
+    $(this).find(".card-img").removeClass("moon")
+},function(){
+    $(".card-img").addClass("moon")
+})
+
